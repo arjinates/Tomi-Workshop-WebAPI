4571942f025e1ea632db23877657257a0b8fd911
arzudabaniyasti <dabaniyastiarzu@gmail.com>
2023-05-08 01:11:59 +0300

    Microsoft.AspNetCore.Identity.MongoDbCore is used instead of Microsoft.AspNetCore.Identity

--- Tomi.Domain/Entities/User.cs
+++ Tomi.Domain/Entities/User.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+
 
 namespace Tomi.Domain.Entities
 {
-    public class User : IdentityUser
+    public class User : MongoIdentityUser
     {
         
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
+     
     }
 }
