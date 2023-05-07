59e6a68bb1096b0b01056ecc02c41f604d386bdf
Arjin Ateş <arjinates@hotmail.com>
2023-05-07 16:56:42 +0300

    User entity added

--- Tomi.Domain/Entities/Product.cs
+++ Tomi.Domain/Entities/Product.cs
@@ -1,11 +1,7 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace Tomi.Domain.Entities
+﻿namespace Tomi.Domain.Entities
 {
     public class Product : BaseEntity
-    {
-        
+    { 
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
---
+++ Tomi.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Tomi.Domain.Entities
+{
+    public class User : IdentityUser
+    {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
