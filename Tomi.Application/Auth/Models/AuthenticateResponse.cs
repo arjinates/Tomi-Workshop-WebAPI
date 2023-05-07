cf2c65c448a4a13fd1ad183aa23b07cae4134884
Arjin Ateş <arjinates@hotmail.com>
2023-05-07 16:57:37 +0300

    Auth and Register command models added.

---
+++ Tomi.Application/Auth/Models/AuthenticateResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Tomi.Application.Auth.Models
+{
+    public class AuthenticateResponse
+    {
+        public string Token { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Tomi.Application/Auth/Models/RegisterRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Tomi.Application.ApiResponse;
+
+namespace Tomi.Application.Auth.Models
+{
+    public class RegisterRequest : IRequest<Response<string>>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
