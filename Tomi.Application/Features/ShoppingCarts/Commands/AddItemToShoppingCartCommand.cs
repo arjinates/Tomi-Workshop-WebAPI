e532b8c9c79a29562759ed4c783eb294b9531e75
arzudabaniyasti <dabaniyastiarzu@gmail.com>
2023-05-08 18:23:32 +0300

    added request and response models for shopping cart commands and queries

---
+++ Tomi.Application/Features/ShoppingCarts/Commands/AddItemToShoppingCartCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tomi.Application.ApiResponse;
+using Tomi.Domain.Entities;
+
+namespace Tomi.Application.Features.ShoppingCarts.Commands
+{
+	public class AddItemToShoppingCartCommand : IRequest<ShoppingCartItemModel>
+	{
+		public string UserId { get; set; }
+		public string ProductId { get; set; }
+	}
+}
