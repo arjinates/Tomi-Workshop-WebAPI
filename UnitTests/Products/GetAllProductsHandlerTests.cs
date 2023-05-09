549f259fa0b73cd60e130f3821afddbe53648d42
Arjin Ateş <arjinates@hotmail.com>
2023-05-10 01:17:58 +0300

    ProductHandler Unit Tests added

---
+++ UnitTests/Products/GetAllProductsHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Moq;
+using Tomi.Application.Features.Products.Queries;
+using Tomi.Application.Models;
+using Tomi.Application.Services.Handlers.Products;
+using Tomi.Domain.Entities;
+using Tomi.Domain.IRepositories;
+
+namespace Tomi.Products
+{
+    public class GetAllProductsHandlerTests
+    {
+        [Fact]
+        public async Task Handle_ValidRequest_ReturnsProductModels()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = "1", Name = "Product 1", Price = 50},
+                new Product { Id = "2", Name = "Product 1", Price = 50},
+        };
+            var productModels = new List<ProductModel>
+            {
+                new ProductModel { Id = "1", Name = "Product 1", Price = 50 },
+                new ProductModel { Id = "2", Name = "Product 2", Price = 100 }
+            }; 
+
+            var productRepositoryMock = new Mock<IProductRepository>();
+            var mapperMock = new Mock<IMapper>();
+
+            productRepositoryMock.Setup(repo => repo.GetAllAsync()).ReturnsAsync(products);
+            mapperMock.Setup(m => m.Map<IEnumerable<ProductModel>>(products)).Returns(productModels);
+
+            var handler = new GetAllProductsHandler(productRepositoryMock.Object, mapperMock.Object);
+            var request = new GetAllProductsQuery();
+
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.Equal(productModels, result);
+        }
+    }
+}
---
+++ UnitTests/Products/GetProductByIdHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Moq;
+using Tomi.Application.Features.Products.Queries;
+using Tomi.Application.Models;
+using Tomi.Application.Services.Handlers.Products;
+using Tomi.Domain.Entities;
+using Tomi.Domain.IRepositories;
+
+namespace Tomi.Products
+{
+    public class GetProductByIdHandlerTests
+    {
+        [Fact]
+        public async Task Handle_ValidRequest_ReturnsProductModel()
+        { 
+            var product = new Product { Id = "1", Name = "Product 1", Price = 50 };
+            var productModel = new ProductModel { Id = "1", Name = "Product 1", Price = 50 };
+
+            var productRepositoryMock = new Mock<IProductRepository>();
+            var mapperMock = new Mock<IMapper>();
+
+            productRepositoryMock.Setup(repo => repo.GetByIdAsync(product.Id)).ReturnsAsync(product);
+            mapperMock.Setup(m => m.Map<ProductModel>(product)).Returns(productModel);
+
+            var handler = new GetProductByIdHandler(productRepositoryMock.Object, mapperMock.Object);
+            var request = new GetProductByIdQuery { Id = product.Id };
+
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.Equal(productModel, result);
+        }
+    }
+}
