93f2332c5d1d0091a46b79ce0186222d8705065d
Arjin Ateş <arjinates@hotmail.com>
2023-05-09 11:19:17 +0300

    ApplyCoupon Features added.

---
+++ Tomi.Application/Features/Coupons/Commands/ApplyCouponCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Tomi.Application.Features.Coupons.Commands
+{
+    public class ApplyCouponCommand : IRequest<CouponModel>
+    {
+        public string UserId { get; set; }
+        public string CouponId { get; set; }
+    }
+}
