51e180f3ab9fd568dff7e2a531b3cfb9c2987761
Arjin Ateş <arjinates@hotmail.com>
2023-05-10 01:17:42 +0300

    ShoppingCartHandler Unit Tests Added

---
+++ UnitTests/ShoppingCarts/AddItemToShoppingCartTests.cs
@@ -0,0 +1,69 @@
+﻿using AutoFixture;
+using AutoMapper;
+using Moq;
+using Tomi.Application.Features.ShoppingCarts.Commands;
+using Tomi.Application.Services.Handlers.ShoppingCarts;
+using Tomi.Domain.Entities;
+using Tomi.Domain.IRepositories;
+
+namespace UnitTests.ShoppingCarts
+{
+    public class AddItemToShoppingCartTests
+    {
+        private readonly Mock<IShoppingCartRepository> _mockShoppingCartRepository;
+        private readonly Mock<IProductRepository> _mockProductRepository;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly IFixture _fixture;
+
+        public AddItemToShoppingCartTests()
+        {
+            _mockShoppingCartRepository = new Mock<IShoppingCartRepository>();
+            _mockProductRepository = new Mock<IProductRepository>();
+            _mockMapper = new Mock<IMapper>();
+            _fixture = new Fixture();
+        }
+
+        [Fact] //Test if shoppingCart does not exists
+        public async Task Handle_ShoppingCartDoesNotExist_CreatesNewShoppingCart()
+        {
+            var command = _fixture.Create<AddItemCommand>();
+            var product = _fixture.Create<Product>();
+
+            _mockShoppingCartRepository
+                .Setup(x => x.GetByUserIdAsync(command.UserId))
+                .ReturnsAsync((ShoppingCart)null);
+
+            _mockProductRepository
+                .Setup(x => x.GetByIdAsync(command.ProductId))
+                .ReturnsAsync(product);
+
+            var handler = new AddItemToShoppingCart(_mockShoppingCartRepository.Object, _mockProductRepository.Object, _mockMapper.Object);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            _mockShoppingCartRepository.Verify(x => x.CreateAsync(It.Is<ShoppingCart>(sc => sc.UserId == command.UserId)), Times.Once);
+        }
+
+        [Fact] //Test if shoppingCart exists and adds new items?
+        public async Task Handle_ShoppingCartExists_AddsItemToShoppingCart()
+        {
+            var command = _fixture.Create<AddItemCommand>();
+            var product = _fixture.Create<Product>();
+            var shoppingCart = _fixture.Create<ShoppingCart>();
+
+            _mockShoppingCartRepository
+                .Setup(x => x.GetByUserIdAsync(command.UserId))
+                .ReturnsAsync(shoppingCart);
+
+            _mockProductRepository
+                .Setup(x => x.GetByIdAsync(command.ProductId))
+                .ReturnsAsync(product);
+
+            var handler = new AddItemToShoppingCart(_mockShoppingCartRepository.Object, _mockProductRepository.Object, _mockMapper.Object);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            _mockShoppingCartRepository.Verify(x => x.UpdateAsync(shoppingCart.Id, It.IsAny<ShoppingCart>()), Times.Once);
+        }
+    }
+}
---
+++ UnitTests/ShoppingCarts/RemoveItemFromShoppingCartTests.cs
@@ -0,0 +1,72 @@
+﻿using AutoFixture;
+using AutoMapper;
+using Moq;
+using Tomi.Application.Features.ShoppingCarts.Commands;
+using Tomi.Application.Models;
+using Tomi.Application.Services.Handlers.ShoppingCarts;
+using Tomi.Domain.Entities;
+using Tomi.Domain.IRepositories;
+
+namespace UnitTests.ShoppingCarts
+{
+    public class RemoveItemFromShoppingCartTests
+    {
+        private readonly Mock<IShoppingCartRepository> _shoppingCartRepositoryMock;
+        private readonly Mock<IProductRepository> _productRepositoryMock;
+        private readonly IMapper _mapper;
+        private readonly IFixture _fixture;
+        private readonly RemoveItemFromShoppingCart _removeItemFromShoppingCart;
+
+        public RemoveItemFromShoppingCartTests()
+        {
+            _fixture = new Fixture();
+            _shoppingCartRepositoryMock = new Mock<IShoppingCartRepository>();
+            _productRepositoryMock = new Mock<IProductRepository>();
+            _mapper = new Mapper(new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductModel>()));
+            _removeItemFromShoppingCart = new RemoveItemFromShoppingCart(_shoppingCartRepositoryMock.Object, _productRepositoryMock.Object, _mapper);
+        }
+
+        [Fact]
+        public async Task Handle_ShoppingCartAndItemExist_ReturnsResponse()
+        {
+            var removeItemCommand = _fixture.Create<RemoveItemCommand>();
+            var shoppingCart = _fixture.Create<ShoppingCart>();
+            var product = _fixture.Create<Product>();
+
+            _shoppingCartRepositoryMock.Setup(x => x.GetByUserIdAsync(removeItemCommand.UserId)).ReturnsAsync(shoppingCart);
+            _productRepositoryMock.Setup(x => x.GetByIdAsync(removeItemCommand.ProductId)).ReturnsAsync(product);
+            _shoppingCartRepositoryMock.Setup(x => x.UpdateAsync(shoppingCart.Id, shoppingCart)).Returns(Task.CompletedTask);
+
+            var result = await _removeItemFromShoppingCart.Handle(removeItemCommand, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.Equal(removeItemCommand.UserId, result.UserId);
+            Assert.Equal(removeItemCommand.ProductId, result.ProductId);
+        }
+
+        [Fact]
+        public async Task Handle_ShoppingCartDoesNotExist_ThrowsException()
+        {
+            var removeItemCommand = _fixture.Create<RemoveItemCommand>();
+
+            _shoppingCartRepositoryMock.Setup(x => x.GetByUserIdAsync(removeItemCommand.UserId)).ReturnsAsync((ShoppingCart)null);
+
+            await Assert.ThrowsAsync<InvalidOperationException>(() => _removeItemFromShoppingCart.Handle(removeItemCommand, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task Handle_ShoppingCartItemDoesNotExist_ThrowsException()
+        {
+            var removeItemCommand = _fixture.Create<RemoveItemCommand>();
+            var shoppingCart = _fixture.Create<ShoppingCart>();
+            var product = _fixture.Create<Product>();
+
+            shoppingCart.ShoppingCartItemList.Clear(); // Ensure no items in cart.
+
+            _shoppingCartRepositoryMock.Setup(x => x.GetByUserIdAsync(removeItemCommand.UserId)).ReturnsAsync(shoppingCart);
+            _productRepositoryMock.Setup(x => x.GetByIdAsync(removeItemCommand.ProductId)).ReturnsAsync(product);
+
+            await Assert.ThrowsAsync<InvalidOperationException>(() => _removeItemFromShoppingCart.Handle(removeItemCommand, CancellationToken.None));
+        }
+    }
+}
