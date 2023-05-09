927b8cae8ebd9178fe28bab6eb8713c692e6862f
Arjin Ateş <arjinates@hotmail.com>
2023-05-09 11:17:53 +0300

    Coupon Entity and Repository added.

---
+++ Tomi.Domain/Entities/Coupon.cs
@@ -0,0 +1,9 @@
+﻿namespace Tomi.Domain.Entities
+{
+    public class Coupon : BaseEntity
+    {
+        public string CouponId { get; set; }
+        public string DiscountType { get; set; }
+        public int Value { get; set; }
+    }
+}
