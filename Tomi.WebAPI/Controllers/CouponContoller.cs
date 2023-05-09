71613dcff2fd76ac580a6cf8e91bb1edb0cb0529
Arjin Ateş <arjinates@hotmail.com>
2023-05-09 11:21:16 +0300

    CouponController added.

---
+++ Tomi.WebAPI/Controllers/CouponContoller.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Tomi.Application.Features.Coupons.Commands;
+
+namespace Tomi.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponContoller : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        [HttpPost("apply-coupon")]
+        public async Task<IActionResult> ApplyCouponToShoppingCart(ApplyCouponCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
