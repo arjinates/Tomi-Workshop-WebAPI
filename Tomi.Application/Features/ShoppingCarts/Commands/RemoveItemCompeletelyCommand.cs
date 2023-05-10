3de89799e0d84d98b954dfccd6e5bb9f9ece3de0
Arjin Ateş <arjinates@hotmail.com>
2023-05-10 13:43:20 +0300

    RemoveItemCompeletelyCommand added

---
+++ Tomi.Application/Features/ShoppingCarts/Commands/RemoveItemCompeletelyCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Tomi.Application.ApiResponse;
+
+namespace Tomi.Application.Features.ShoppingCarts.Commands
+{
+    public class RemoveItemCompeletelyCommand : IRequest<Response<string>>
+    {
+        public string UserId { get; set; }
+        public string ProductId { get; set; }
+    }
+}
